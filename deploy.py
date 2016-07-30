bc9df00b438ff6e2f7045aa52026fa5c8e8015cd
Yuri Vishnevsky <yurivish@gmail.com>
2016-07-30 14:14:27 -0400

    Fix another typo

--- deploy.py
+++ deploy.py
@@ -16,7 +16,7 @@ def main():
 		call(['git', 'worktree', 'add', '-b', 'gh-pages', 'public', 'origin/gh-pages'], cwd=relpath())
 
 	# Build assets
-	built_assets.main()
+	build_assets.main()
 
 	public = relpath('public')
 
