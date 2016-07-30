2ff04c12ca25f81c9b92311b23b7d2bbc615fe86
Yuri Vishnevsky <yurivish@gmail.com>
2016-07-30 15:12:04 -0400

    Right. Let's just keep using S3.

--- deploy.py
+++ deploy.py
@@ -5,28 +5,10 @@ from subprocess import call, check_output
 from build_assets import relpath
 import build_assets
 
+# TODO: Just use s3cmd.
+
 def main():
-	if os.path.exists('dist'):
-		shutil.rmtree(relpath('dist'))
-
-	# Build assets
-	build_assets.main()
-
-	call(['git', 'branch', '-D', 'gh-pages'])
-	call(['git', 'checkout', '--orphan', 'gh-pages'])
-	call(['git', 'reset'])
-	call(['git', 'commit', '--allow-empty', '-m', 'Initial commit'])
-	call(['git', 'checkout', '--force', 'master'])
-	call(['git', 'worktree', 'add', 'dist', 'gh-pages'])
-	call(['hugo'])
-	call(['cp', '-a', 'public/.', 'dist'])
-
-	call(['git', 'add', '--all'], cwd=relpath('dist'))
-	call(['git', 'commit', '-m', 'Deploy.'], cwd=relpath('dist'))
-	call(['git', 'push', '--force', '--set-upstream', 'origin', 'gh-pages'], cwd=relpath('dist'))
-
-	# shutil.rmtree(relpath('dist'))
-	# rm dist
+	
 
 
 if __name__ == '__main__':
