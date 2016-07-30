8af7497ba3ee151a6ac702d5f7c0385a2b9387b4
Yuri Vishnevsky <yurivish@gmail.com>
2016-07-30 13:54:35 -0400

    Update deploy script

--- deploy.py
+++ deploy.py
@@ -2,14 +2,10 @@
 
 import os
 from subprocess import call, check_output
-
-def relpath(path):
-	'''Return a version of `path` relative to the directory of this script.'''
-	here = os.path.dirname(os.path.realpath(__file__))
-	return os.path.join(here, path)
+from build_assets import relpath
+import build_assets
 
 def main():
-
 	assert(len(check_output(['git', 'status', '--porcelain'], cwd=relpath(''))) == 0)
 
 	public = relpath('public')
